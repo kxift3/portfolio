6d648b39738a1382c796d4274a1111ab252ae7ec
kaif tahir <kaifyta123@gmail.com>
2024-12-29 10:31:41 +0000

    batting average

---
+++ practicals/01/batting_average.py
@@ -0,0 +1,8 @@
+matches = 609
+times_batted = 1014
+not_out = 162
+runs_scored = 48426
+
+completed_innings = times_batted - not_out
+batting_average = runs_scored / completed_innings
+print(f"Boycott's batting average is: {batting_average:.2f}")
