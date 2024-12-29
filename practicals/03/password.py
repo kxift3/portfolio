67db9dc1e0cec55468142133b7457c75ca6e20ac
kaif tahir <kaifyta123@gmail.com>
2024-12-29 10:39:43 +0000

    password

---
+++ practicals/03/password.py
@@ -0,0 +1,7 @@
+password1 = input("Enter a new password: ")
+password2 = input("Confirm your password: ")
+
+if password1 == password2:
+    print("Password Set")
+else:
+    print("Error: Passwords do not match")
