157d2cacc39a8daa8600ffae63d9503241f26cb2
kxift3 <kaifyta123@gmail.com>
2025-01-08 12:07:12 +0000

    .

--- practicals/07/unique_letters.py
+++
@@ -1,6 +0,0 @@
-def get_unique_sorted_letters(string):
-    return sorted(set(string))
-
-string = input("Enter a string: ")
-unique_letters = get_unique_sorted_letters(string)
-print(f"Unique letters: {unique_letters}")
