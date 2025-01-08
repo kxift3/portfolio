45e3535f92ad8362fc4c89807162b57e653fb1f5
kxift3 <kaifyta123@gmail.com>
2025-01-08 13:32:49 +0000

    .

--- practicals/04/capitalise_name.py
+++
@@ -1,6 +0,0 @@
-def format_name(name):
-    return name.capitalize()
-
-name = input("Enter your name: ")
-formatted_name = format_name(name)
-print(f"Hello, {formatted_name}!")
--- practicals/04/input_validation.py
+++
@@ -1,8 +0,0 @@
-def is_valid_number(num):
-    return 0 <= num <= 100
-
-number = int(input("Enter a number between 0 and 100: "))
-if is_valid_number(number):
-    print("Valid number!")
-else:
-    print("Invalid number!")
