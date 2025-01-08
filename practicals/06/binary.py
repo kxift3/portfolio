f9eb713d4f08de0a8d60e5155bcab2c6ce7b6132
kxift3 <kaifyta123@gmail.com>
2025-01-08 13:33:46 +0000

    .

--- practicals/06/binary.py
+++
@@ -1,8 +0,0 @@
-def to_binary(number):
-    return bin(number)[2:]
-
-number = int(input("Enter a positive integer: "))
-if number > 0:
-    print(f"Binary representation of {number}: {to_binary(number)}")
-else:
-    print("Please enter a positive integer.")
--- practicals/06/encription.py
+++
@@ -1,6 +0,0 @@
-def encrypt_message(message):
-    return message.replace(" ", "")[::-1]
-
-message = input("Enter a message to encrypt: ")
-encrypted = encrypt_message(message)
-print(f"Encrypted message: {encrypted}")
